8389ea0ffb9e13af5f03c1baf1a14e536319d8bc
saxonsgrammar <gramatikvlad1986@gmail.com>
2023-10-19 19:29:08 +0300

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace wpf3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        DispatcherTimer timer;
+        public MainWindow()
+        {
+            InitializeComponent();
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(500);
+            timer.Tick += Timer_Tick;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            timelineSlider.Value = media.Position.TotalSeconds;
+        }
+
+        private void Play_Click(object sender, RoutedEventArgs e)
+        {
+            media.Play();
+        }
+
+        private void Pause_Click(object sender, RoutedEventArgs e)
+        {
+            media.Pause();
+        }
+
+        private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+            media.Stop();
+        }
+
+        private void Element_MediaOpened(object sender, EventArgs e)
+        {
+            TimeSpan ts = media.NaturalDuration.TimeSpan;
+            timelineSlider.Maximum = ts.TotalSeconds;
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            media.Position = TimeSpan.FromSeconds(timelineSlider.Value);
+        }
+    }
+}
